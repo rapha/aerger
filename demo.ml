5e110dca8e5cb7e8b89c7a39e89f1c0b6437d246
Raphael Speyer <rspeyer@gmail.com>
2012-05-08 22:13:11 +1000

    renamed get_or_default to get_or

--- demo.ml
+++ demo.ml
@@ -1,7 +1,7 @@
 let _ =
   let awesomeness, colour, speed = Aerger.(
     require (float ~name:"awesomeness" ~desc:"How awesome to be."),
-    get_or_default "RED" (enum ~name:"colour" ~desc:"A primary colour." ~values:["RED"; "GREEN"; "BLUE"]),
+    get_or "RED" (enum ~name:"colour" ~desc:"A primary colour." ~values:["RED"; "GREEN"; "BLUE"]),
     match get (bool "go_faster" "Whether to go faster") with Some true -> "quickly" | Some false -> "slowly" | None -> "?"
   ) in
   Printf.printf "speed: %s\nawesomeness: %f\ncolour: %s\n" speed awesomeness colour;
--- test.ml
+++ test.ml
@@ -4,8 +4,8 @@ let _ =
   let get argv =
     let module A = Aerger.Of(struct let argv = (Array.of_list ("cmd" :: argv)) end) in A.get
   in
-  let get_or_default argv =
-    let module A = Aerger.Of(struct let argv = (Array.of_list ("cmd" :: argv)) end) in A.get_or_default
+  let get_or argv =
+    let module A = Aerger.Of(struct let argv = (Array.of_list ("cmd" :: argv)) end) in A.get_or
   in
   let require argv =
     let module A = Aerger.Of(struct let argv = (Array.of_list ("cmd" :: argv)) end) in A.require
@@ -18,8 +18,8 @@ let _ =
   assert (get ["value"; "-some"] some_arg = None);
   assert (get ["-some"; "value"] some_arg = Some "value");
   assert (get ["-color"; "red"] (Aerger.enum "color" "a color" ["red"; "green"; "blue"]) = Some "red");
-  assert (get_or_default [] "otherwise" some_arg = "otherwise");
-  assert (get_or_default ["-some"; "value"] "otherwise" some_arg = "value");
+  assert (get_or [] "otherwise" some_arg = "otherwise");
+  assert (get_or ["-some"; "value"] "otherwise" some_arg = "value");
   assert (try (ignore (require [] some_arg); false) with _ -> true);
   assert (require ["-some"; "value"] some_arg = "value");
   assert (try (ignore (get ["-num"; "not a number"] (Aerger.float "num" "not a number")); false) with _ -> true);
